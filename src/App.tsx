582a0c4c2e45f1c5e2f8697526570795183b0e4d
Robert Octavian Popescu <popescu.robert.octavian@gmail.com>
2022-09-06 22:45:28 +0200

    move theme provider and const [mode, setMode] = React.useState<'light' | 'dark'>('light');

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
+import * as React from 'react';
 import { Canvas } from "@react-three/fiber";
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 import Box from "./components/Box";
 import Header from "./components/Header";
 import "./App.css";
 
 function App() {
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+
+      <CssBaseline />
       <Header />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Canvas>
@@ -14,7 +43,7 @@ function App() {
         <Box position={[-1.2, 0, 0]} />
         <Box position={[1.2, 0, 0]} />
       </Canvas>
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 import App from "./App";
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#1976d2',
-    },
-  },
-});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
