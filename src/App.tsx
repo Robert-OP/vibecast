44bf1a9383d43e2cfcdfdf65255824e4b32e24be
Robert Octavian Popescu <popescu.robert.octavian@gmail.com>
2022-09-06 21:50:40 +0200

    add tailwindcss and drei

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import "./App.css";
 
 function App() {
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </>
   );
 }
 
